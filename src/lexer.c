c9c3708527181143806577a594ad1ca7c80320ac
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 17:15:58 +0300

    Update lexer.c

--- src/lexer.c
+++ src/lexer.c
@@ -1,95 +1,158 @@
-/*
-static const char* keywords[] = {
-    "CLASS", "DEFINITION", "PUBLIC", "SECTION", "METHODS", "IMPORTING", "TYPE",
-    "RETURNING", "VALUE", "ENDCLASS", "IMPLEMENTATION", "METHOD", "DATA",
-    "IF", "RAISE", "EXCEPTION", "ENDIF", "ENDMETHOD"
-};
-*/
-
+#include "lexer.h"
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "lexer.h"
 
-static const char* src_ptr;
-static token_t current_token;
+#define MAX_TOKEN_SIZE 256
 
-void lexer_init(const char* source) {
-    src_ptr = source;
-}
+// === Ключевые слова ABAP (можно заменить загрузкой из файла) ===
+static const char* keywords[] = {
+    "DATA", "TYPES", "CLASS", "METHOD", "ENDCLASS", "IF", "ENDIF", "PERFORM", "CALL",
+    "FUNCTION", "ENDFUNCTION", "LOOP", "ENDLOOP", "DO", "ENDDO", "SELECT", "FROM", "WHERE",
+    "RAISE", "MODULE", "ENDMODULE", "FORM", "ENDFORM", "WRITE", "READ", "MOVE", "CLEAR",
+    "APPEND", "DELETE", "EXPORTING", "IMPORTING", "CHANGING", "RAISING", "TRY", "CATCH",
+    "ENDTRY", "CONTINUE", "STOP", "AND", "OR", "NOT", "INTO", "NEW", "RETURNING"
+};
 
-static void skip_whitespace() {
-    while (*src_ptr && isspace(*src_ptr)) src_ptr++;
-}
+#define KEYWORDS_COUNT (sizeof(keywords) / sizeof(keywords[0]))
 
 static bool is_keyword(const char* str) {
-    // Минимальный набор ключевых слов для примера
-    const char* keywords[] = {"IF", "ELSE", "WHILE", "FOR", "FUNCTION", "RETURN", NULL};
-    for (int i = 0; keywords[i]; i++) {
-        if (strcmp(str, keywords[i]) == 0) return true;
+    for (size_t i = 0; i < KEYWORDS_COUNT; i++) {
+        if (strcasecmp(str, keywords[i]) == 0) return true;
     }
     return false;
 }
 
-token_t lexer_next_token() {
-    skip_whitespace();
+// === Лексер ===
 
-    if (*src_ptr == '\0') {
-        current_token.type = TOKEN_EOF;
-        current_token.text[0] = '\0';
-        return current_token;
+void lexer_init(lexer_t* lexer, const char* source) {
+    lexer->source = source;
+    lexer->pos = 0;
+    lexer->line = 1;
+    lexer->column = 1;
+}
+
+static char peek(lexer_t* lexer) {
+    return lexer->source[lexer->pos];
+}
+
+static char peek_next(lexer_t* lexer) {
+    return lexer->source[lexer->pos + 1];
+}
+
+static char advance(lexer_t* lexer) {
+    char c = lexer->source[lexer->pos++];
+    if (c == '\n') {
+        lexer->line++;
+        lexer->column = 1;
+    } else {
+        lexer->column++;
+    }
+    return c;
+}
+
+static token_t make_token(token_type_t type, const char* start, size_t len, int line, int col) {
+    char* lexeme = (char*)malloc(len + 1);
+    strncpy(lexeme, start, len);
+    lexeme[len] = '\0';
+    return (token_t){ type, lexeme, line, col };
+}
+
+void lexer_free_token(token_t* token) {
+    if (token->lexeme) {
+        free(token->lexeme);
+        token->lexeme = NULL;
+    }
+}
+
+token_t lexer_next_token(lexer_t* lexer) {
+    while (isspace(peek(lexer))) advance(lexer);
+
+    int line = lexer->line;
+    int col = lexer->column;
+    char c = peek(lexer);
+
+    if (c == '\0') return make_token(TOKEN_EOF, "", 0, line, col);
+
+    // Комментарии
+    if (c == '*' || c == '"') {
+        while (peek(lexer) != '\n' && peek(lexer) != '\0') advance(lexer);
+        return lexer_next_token(lexer); // пропустить комментарий
     }
 
-    // Идентификатор или ключевое слово
-    if (isalpha(*src_ptr) || *src_ptr == '_') {
-        int len = 0;
-        while (isalnum(src_ptr[len]) || src_ptr[len] == '_') {
-            if (len < MAX_TOKEN_LEN - 1) len++;
-            else break;
-        }
-        strncpy(current_token.text, src_ptr, len);
-        current_token.text[len] = '\0';
-        src_ptr += len;
-
-        if (is_keyword(current_token.text)) {
-            current_token.type = TOKEN_KEYWORD;
-        } else {
-            current_token.type = TOKEN_IDENTIFIER;
-        }
-        return current_token;
+    // Идентификаторы / ключевые слова
+    if (isalpha(c) || c == '_') {
+        const char* start = &lexer->source[lexer->pos];
+        while (isalnum(peek(lexer)) || peek(lexer) == '_') advance(lexer);
+        size_t len = &lexer->source[lexer->pos] - start;
+        return make_token(is_keyword(start) ? TOKEN_KEYWORD : TOKEN_IDENTIFIER, start, len, line, col);
     }
 
-    // Число (только целое)
-    if (isdigit(*src_ptr)) {
-        int len = 0;
-        while (isdigit(src_ptr[len])) {
-            if (len < MAX_TOKEN_LEN - 1) len++;
-            else break;
+    // Числа (целые, десятичные, научные)
+    if (isdigit(c)) {
+        const char* start = &lexer->source[lexer->pos];
+        bool dot_found = false;
+        while (isdigit(peek(lexer)) || (!dot_found && peek(lexer) == '.')) {
+            if (peek(lexer) == '.') dot_found = true;
+            advance(lexer);
         }
-        strncpy(current_token.text, src_ptr, len);
-        current_token.text[len] = '\0';
-        src_ptr += len;
-        current_token.type = TOKEN_NUMBER;
-        return current_token;
+        if (tolower(peek(lexer)) == 'e') {
+            advance(lexer);
+            if (peek(lexer) == '+' || peek(lexer) == '-') advance(lexer);
+            while (isdigit(peek(lexer))) advance(lexer);
+        }
+        size_t len = &lexer->source[lexer->pos] - start;
+        return make_token(TOKEN_NUMBER, start, len, line, col);
     }
 
-    // Строка в кавычках (")
-    if (*src_ptr == '"') {
-        src_ptr++;
-        int len = 0;
-        while (*src_ptr && *src_ptr != '"' && len < MAX_TOKEN_LEN - 1) {
-            current_token.text[len++] = *src_ptr++;
+    // Строки в одинарных кавычках с экранированием
+    if (c == '\'') {
+        advance(lexer); // начальная кавычка
+        const char* start = &lexer->source[lexer->pos];
+        size_t len = 0;
+        while (peek(lexer) != '\0') {
+            if (peek(lexer) == '\'' && peek_next(lexer) == '\'') {
+                advance(lexer); advance(lexer); // двойная кавычка = экранирование
+                len += 1;
+            } else if (peek(lexer) == '\'') {
+                break;
+            } else {
+                advance(lexer);
+                len++;
+            }
         }
-        current_token.text[len] = '\0';
-        if (*src_ptr == '"') src_ptr++;
-        current_token.type = TOKEN_STRING;
-        return current_token;
+        advance(lexer); // закрывающая кавычка
+        return make_token(TOKEN_STRING, start, len, line, col);
     }
 
-    // Операторы (одиночные символы)
-    current_token.text[0] = *src_ptr;
-    current_token.text[1] = '\0';
-    current_token.type = TOKEN_OPERATOR;
-    src_ptr++;
-    return current_token;
+    // Операторы
+    if (c == '=' && peek_next(lexer) == '>') {
+        advance(lexer); advance(lexer);
+        return make_token(TOKEN_OPERATOR, "=>", 2, line, col);
+    }
+    if (c == '-' && peek_next(lexer) == '>') {
+        advance(lexer); advance(lexer);
+        return make_token(TOKEN_OPERATOR, "->", 2, line, col);
+    }
+    if (c == '<' && peek_next(lexer) == '>') {
+        advance(lexer); advance(lexer);
+        return make_token(TOKEN_OPERATOR, "<>", 2, line, col);
+    }
+    if ((c == '<' || c == '>') && peek_next(lexer) == '=') {
+        char op[3] = { c, '=', '\0' };
+        advance(lexer); advance(lexer);
+        return make_token(TOKEN_OPERATOR, op, 2, line, col);
+    }
+
+    // Символы
+    if (strchr("+-*/=()[],.;:", c)) {
+        advance(lexer);
+        return make_token(TOKEN_SYMBOL, &lexer->source[lexer->pos - 1], 1, line, col);
+    }
+
+    // Неизвестный символ
+    fprintf(stderr, "[LEXER ERROR] Unknown character '%c' at %d:%d\n", c, line, col);
+    advance(lexer);
+    return make_token(TOKEN_UNKNOWN, &lexer->source[lexer->pos - 1], 1, line, col);
 }
