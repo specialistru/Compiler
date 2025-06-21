f6f255b25fc2c9a335fa71778efac26e09304fa8
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 15:37:58 +0300

    Update optimizer.c

--- src/optimizer.c
+++ src/optimizer.c
@@ -1,1 +1,16 @@
+#include "optimizer.h"
+#include <string.h>
+#include <stdio.h>
+
+int optimize(Instruction* code, int n) {
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        if (i > 0 && code[i].op == code[i-1].op && strcmp(code[i].arg1, code[i-1].arg1) == 0) {
+            // Пропускаем дубликат
+            continue;
+        }
+        code[j++] = code[i];
+    }
+    return j;
+}
 
