530130c556f417d456a1693e51e5c5ac0d9e70bc
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 19:30:40 +0300

    Update parser_if_elseif.c

--- src/parser_if_elseif.c
+++ src/parser_if_elseif.c
@@ -1,1 +1,21 @@
+// parser_if_elseif.c
+#include "parser_if_elseif.h"
+#include "ast.h"
+#include "parser_if_condition.h"
+#include "parser_if_body.h"
+#include "parser.h"
 
+bool parse_if_elseif(parser_t* parser, ast_node_t* parent_node) {
+    // Создаем узел ELSEIF
+    token_t token = parser_previous_token(parser);
+    ast_node_t* elseif_node = ast_node_create(AST_ELSEIF, token);
+
+    // Парсим условие ELSEIF
+    if (!parse_if_condition(parser, elseif_node)) return false;
+
+    // Парсим тело ELSEIF
+    if (!parse_if_body(parser, elseif_node)) return false;
+
+    ast_node_add_child(parent_node, elseif_node);
+    return true;
+}
