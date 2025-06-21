24b59bc3274d42962ce2bb866ccb86a33f4b0087
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 16:19:05 +0300

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -9,4 +9,23 @@
 #define MAX_PARAM_VALUE_LEN 64
 #define MAX_PARAM_KEY_LEN 4
 
+// Лексер - максимальная длина токена
+#define MAX_TOKEN_LEN 128
+
+// Опкоды IR
+typedef enum {
+    IR_NOP,
+    IR_LOAD_CONST,
+    IR_LOAD_VAR,
+    IR_STORE_VAR,
+    IR_ADD,
+    IR_SUB,
+    IR_MUL,
+    IR_DIV,
+    IR_JUMP,
+    IR_JUMP_IF_FALSE,
+    IR_CALL,
+    IR_RET
+} ir_opcode_t;
+
 #endif // CONSTANTS_H
