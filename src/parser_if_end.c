8e729a8023be9401e3283b767992f64f9249f675
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 19:31:07 +0300

    Update parser_if_end.c

--- src/parser_if_end.c
+++ src/parser_if_end.c
@@ -1,1 +1,11 @@
+// parser_if_end.c
+#include "parser_if_end.h"
+#include "parser.h"
 
+bool parse_if_end(parser_t* parser) {
+    if (parser_match_keyword(parser, "ENDIF")) {
+        return true;
+    }
+    parser_error(parser, "Ожидался ENDIF для завершения конструкции IF");
+    return false;
+}
