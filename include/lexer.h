ef25749fbb461c88a6a7651273a11a9bca2f8d46
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 15:34:56 +0300

    Update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -1,1 +1,22 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#define MAX_TOKENS 1024
+
+typedef enum {
+    TOKEN_KEYWORD,
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING,
+    TOKEN_SYMBOL,
+    TOKEN_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char text[64];
+} Token;
+
+int lex(const char* input, Token* tokens, int max_tokens);
+
+#endif
 
