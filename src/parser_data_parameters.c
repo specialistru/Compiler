d787d16ec5ac9fd33179817ed3b8c7b93833e3a7
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 20:43:48 +0300

    Update parser_data_parameters.c

--- src/parser_data_parameters.c
+++ src/parser_data_parameters.c
@@ -1,9 +1,7 @@
 // parser_data_parameters.c
 #include "parser_data_parameters.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-// Вспомогательная функция: ожидание идентификатора
 static token_t expect_identifier(parser_context_t* ctx) {
     if (ctx->current.type != TOKEN_IDENTIFIER) {
         fprintf(stderr, "[PARSER ERROR] Ожидался идентификатор, но найдено: %s\n", ctx->current.lexeme);
@@ -14,39 +12,27 @@ static token_t expect_identifier(parser_context_t* ctx) {
     return token;
 }
 
-// Парсинг PARAMETERS <param_name> TYPE <type_name>.
-ast_node_t* parse_parameters_declaration(parser_context_t* ctx) {
+ast_node_t* parse_data_parameters(parser_context_t* ctx) {
     if (!parser_match(ctx, TOKEN_KEYWORD, "PARAMETERS")) {
-        return NULL; // не PARAMETERS
+        return NULL;
     }
 
-    token_t decl_tok = ctx->previous;
-    ast_node_t* node = ast_node_create(AST_PARAMETERS, decl_tok);
+    ast_node_t* node = ast_node_create(AST_PARAMETERS, ctx->previous);
 
-    // Имя параметра
-    token_t param_name_tok = expect_identifier(ctx);
-    ast_node_t* param_name_node = ast_node_create(AST_EXPR_IDENTIFIER, param_name_tok);
-    ast_node_add_child(node, param_name_node);
+    token_t ident_tok = expect_identifier(ctx);
+    ast_node_t* ident_node = ast_node_create(AST_EXPR_IDENTIFIER, ident_tok);
+    ast_node_add_child(node, ident_node);
 
-    // Ключевое слово TYPE (обязательно для PARAMETERS)
-    if (!parser_match(ctx, TOKEN_KEYWORD, "TYPE")) {
-        fprintf(stderr, "[PARSER ERROR] Ожидалось ключевое слово TYPE после имени параметра в PARAMETERS\n");
-        exit(EXIT_FAILURE);
+    // Опциональный TYPE <typename>
+    if (parser_match(ctx, TOKEN_KEYWORD, "TYPE")) {
+        token_t type_tok = expect_identifier(ctx);
+        ast_node_t* type_node = ast_node_create(AST_EXPR_IDENTIFIER, type_tok);
+        ast_node_add_child(node, type_node);
     }
 
-    // Тип параметра
-    if (ctx->current.type != TOKEN_IDENTIFIER) {
-        fprintf(stderr, "[PARSER ERROR] Ожидался идентификатор типа после TYPE в PARAMETERS\n");
-        exit(EXIT_FAILURE);
-    }
-    token_t type_tok = ctx->current;
-    parser_advance(ctx);
-    ast_node_t* type_node = ast_node_create(AST_EXPR_IDENTIFIER, type_tok);
-    ast_node_add_child(node, type_node);
-
-    // Проверка точки в конце
+    // Ожидаем точку в конце
     if (!parser_match(ctx, TOKEN_SYMBOL, ".")) {
-        fprintf(stderr, "[PARSER ERROR] Ожидалась '.' в конце PARAMETERS объявления\n");
+        fprintf(stderr, "[PARSER ERROR] Ожидалась точка '.' в конце объявления PARAMETERS\n");
         exit(EXIT_FAILURE);
     }
 
