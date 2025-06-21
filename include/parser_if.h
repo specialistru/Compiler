ac211e2dac9dacea5aa4d06d6db299434d39b249
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 19:44:15 +0300

    Update parser_if.h

--- include/parser_if.h
+++ include/parser_if.h
@@ -1,11 +1,10 @@
 #ifndef PARSER_IF_H
 #define PARSER_IF_H
 
-#include "parser_context.h"
 #include "ast.h"
 #include "lexer.h"
 
-// Парсинг конструкции IF ... ELSEIF ... ELSE ... ENDIF
-ast_node_t* parse_if_statement(parser_context_t* ctx);
+// Главная функция для парсинга конструкции IF
+ast_node_t* parse_if(token_stream_t *tokens);
 
 #endif // PARSER_IF_H
