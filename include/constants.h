97a9e5dded2f088f57b389ce2ea4f3ece920dfc7
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 16:10:43 +0300

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -1,1 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
 
+#define PF_STATUS_100 "100"
+#define PARAM_ID_AAT "AAT"
+#define PARAM_ID_WRK "WRK"
+
+// Максимальные размеры
+#define MAX_PARAM_VALUE_LEN 64
+#define MAX_PARAM_KEY_LEN 4
+
+#endif // CONSTANTS_H
