036739b31fad4b653ad0f411d9a4ebf1871d2ac6
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 16:28:02 +0300

    Update interpreter.c

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include "interpreter.h"
+#include "ir.h"
+
+void interpreter_run() {
+    printf("Running IR interpreter...\n");
+    ir_print();
+    // Здесь могла бы быть реализация интерпретации IR
+}
 
