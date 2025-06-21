18605be8ec6d77a00f14d2f37f9de2e2e42ace43
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 19:30:53 +0300

    Update parser_if_else.c

--- src/parser_if_else.c
+++ src/parser_if_else.c
@@ -1,1 +1,15 @@
+// parser_if_else.c
+#include "parser_if_else.h"
+#include "ast.h"
+#include "parser_if_body.h"
+#include "parser.h"
 
+bool parse_if_else(parser_t* parser, ast_node_t* parent_node) {
+    token_t token = parser_previous_token(parser);
+    ast_node_t* else_node = ast_node_create(AST_ELSE, token);
+
+    if (!parse_if_body(parser, else_node)) return false;
+
+    ast_node_add_child(parent_node, else_node);
+    return true;
+}
