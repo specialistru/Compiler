7dc8fc8c4547c22be3062f33f678ef60c6eede6c
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-21 16:24:18 +0300

    Update lexer.c

--- src/lexer.c
+++ src/lexer.c
@@ -1,64 +1,95 @@
-#include "lexer.h"
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
+/*
 static const char* keywords[] = {
     "CLASS", "DEFINITION", "PUBLIC", "SECTION", "METHODS", "IMPORTING", "TYPE",
     "RETURNING", "VALUE", "ENDCLASS", "IMPLEMENTATION", "METHOD", "DATA",
     "IF", "RAISE", "EXCEPTION", "ENDIF", "ENDMETHOD"
 };
-static const int keywords_count = sizeof(keywords)/sizeof(keywords[0]);
+*/
 
-static int is_keyword(const char* s) {
-    for (int i=0; i<keywords_count; i++) {
-        if (strcmp(s, keywords[i]) == 0) return 1;
-    }
-    return 0;
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include "lexer.h"
+
+static const char* src_ptr;
+static token_t current_token;
+
+void lexer_init(const char* source) {
+    src_ptr = source;
 }
 
-int lex(const char* input, Token* tokens, int max_tokens) {
-    int pos = 0, token_count = 0;
-
-    while (input[pos] != '\0' && token_count < max_tokens) {
-        while (isspace(input[pos])) pos++;
-
-        if (input[pos] == '\0') break;
-
-        if (isalpha(input[pos]) || input[pos] == '_' || input[pos] == '>' || input[pos] == '-') {
-            int start = pos;
-            while (isalnum(input[pos]) || input[pos] == '_' || input[pos] == '>' || input[pos] == '-') pos++;
-            int len = pos - start;
-            if (len > 63) len = 63;
-            strncpy(tokens[token_count].text, &input[start], len);
-            tokens[token_count].text[len] = '\0';
-            tokens[token_count].type = is_keyword(tokens[token_count].text) ? TOKEN_KEYWORD : TOKEN_IDENTIFIER;
-            token_count++;
-            continue;
-        }
-
-        if (input[pos] == '\'') {
-            pos++;
-            int start = pos;
-            while (input[pos] != '\'' && input[pos] != '\0') pos++;
-            int len = pos - start;
-            if (len > 63) len = 63;
-            strncpy(tokens[token_count].text, &input[start], len);
-            tokens[token_count].text[len] = '\0';
-            tokens[token_count].type = TOKEN_STRING;
-            token_count++;
-            if (input[pos] == '\'') pos++;
-            continue;
-        }
-
-        tokens[token_count].text[0] = input[pos];
-        tokens[token_count].text[1] = '\0';
-        tokens[token_count].type = TOKEN_SYMBOL;
-        token_count++;
-        pos++;
-    }
-    tokens[token_count].type = TOKEN_EOF;
-    tokens[token_count].text[0] = '\0';
-    return token_count;
+static void skip_whitespace() {
+    while (*src_ptr && isspace(*src_ptr)) src_ptr++;
 }
 
+static bool is_keyword(const char* str) {
+    // Минимальный набор ключевых слов для примера
+    const char* keywords[] = {"IF", "ELSE", "WHILE", "FOR", "FUNCTION", "RETURN", NULL};
+    for (int i = 0; keywords[i]; i++) {
+        if (strcmp(str, keywords[i]) == 0) return true;
+    }
+    return false;
+}
+
+token_t lexer_next_token() {
+    skip_whitespace();
+
+    if (*src_ptr == '\0') {
+        current_token.type = TOKEN_EOF;
+        current_token.text[0] = '\0';
+        return current_token;
+    }
+
+    // Идентификатор или ключевое слово
+    if (isalpha(*src_ptr) || *src_ptr == '_') {
+        int len = 0;
+        while (isalnum(src_ptr[len]) || src_ptr[len] == '_') {
+            if (len < MAX_TOKEN_LEN - 1) len++;
+            else break;
+        }
+        strncpy(current_token.text, src_ptr, len);
+        current_token.text[len] = '\0';
+        src_ptr += len;
+
+        if (is_keyword(current_token.text)) {
+            current_token.type = TOKEN_KEYWORD;
+        } else {
+            current_token.type = TOKEN_IDENTIFIER;
+        }
+        return current_token;
+    }
+
+    // Число (только целое)
+    if (isdigit(*src_ptr)) {
+        int len = 0;
+        while (isdigit(src_ptr[len])) {
+            if (len < MAX_TOKEN_LEN - 1) len++;
+            else break;
+        }
+        strncpy(current_token.text, src_ptr, len);
+        current_token.text[len] = '\0';
+        src_ptr += len;
+        current_token.type = TOKEN_NUMBER;
+        return current_token;
+    }
+
+    // Строка в кавычках (")
+    if (*src_ptr == '"') {
+        src_ptr++;
+        int len = 0;
+        while (*src_ptr && *src_ptr != '"' && len < MAX_TOKEN_LEN - 1) {
+            current_token.text[len++] = *src_ptr++;
+        }
+        current_token.text[len] = '\0';
+        if (*src_ptr == '"') src_ptr++;
+        current_token.type = TOKEN_STRING;
+        return current_token;
+    }
+
+    // Операторы (одиночные символы)
+    current_token.text[0] = *src_ptr;
+    current_token.text[1] = '\0';
+    current_token.type = TOKEN_OPERATOR;
+    src_ptr++;
+    return current_token;
+}
